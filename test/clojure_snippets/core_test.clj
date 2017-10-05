df785a9826c6cab7e06bae766536a6970b5050fb
Jakub Gorczyca <shellbro@users.noreply.github.com>
2017-10-05 11:56:58 +0200

    initial commit

---
+++ test/clojure_snippets/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-snippets.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-snippets.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
