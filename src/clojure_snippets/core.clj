b2b4b61b271260d5a2827713fb56e9447d360e5f
Jakub Gorczyca <shellbro@users.noreply.github.com>
2017-10-19 12:34:56 +0200

    add String snippets

--- src/clojure_snippets/core.clj
+++ src/clojure_snippets/core.clj
@@ -1,6 +1,15 @@
 (ns clojure-snippets.core
   (:gen-class))
 
+                                        ; parse String (or anything) into Number (BigDecimal)
+(bigdec "12.34")
+
+                                        ; cast anything to String
+(str 12.34M)
+
+                                        ; append/prepend to a String
+(str 1 "foo" \a)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
