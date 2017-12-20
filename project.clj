ef3f6683ede36d767e33e55730cbad329f31a9f5
Jakub Gorczyca <shellbro@users.noreply.github.com>
2017-12-20 11:15:25 +0100

    add dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[clj-time "0.14.2"]
+                 [org.clojure/clojure "1.9.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot clojure-snippets.core
   :target-path "target/%s"
